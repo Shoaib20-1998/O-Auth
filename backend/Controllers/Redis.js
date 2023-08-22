ce3825d1778731e3bcf99369067f1e80e5ba4399
Your Name <sm920149@gmail.com>
2023-08-22 20:03:25 +0530

    backend done

--- backend/Controllers/Redis.js
+++ backend/Controllers/Redis.js
@@ -1,5 +1,33 @@
 const redis = require('redis');
-const redisClient = redis.createClient({
-    host: 'localhost', // Change this to your Redis host
-    port: 6379,        // Change this to your Redis port
+require('dotenv').config()
+const redisHost = process.env.REDIS_HOST || '127.0.0.1';
+const redisPort = process.env.REDIS_PORT || 6379;
+const Client = redis.createClient({
+    host: redisHost,
+    port: redisPort        
 });
+
+Client.on('connect',()=>{
+    console.log("client is connected to redis")
+})
+
+Client.on('error',(err)=>{
+    console.log(err.message)
+})
+
+Client.on('ready',(err)=>{
+    console.log("client is connected to redis and ready to use")
+
+})
+Client.on('end',(err)=>{
+    console.log("client disconnected to redis")
+})
+
+process.on('SIGINT',()=>{
+    Client.quit()
+})
+
+
+
+module.exports=Client
+
--- backend/Controllers/passport-config.js
+++ backend/Controllers/passport-config.js
@@ -1,50 +1,29 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const { db } = require('../db.config.js'); // Import the DynamoDB configuration
 
-passport.use(new LocalStrategy(
-    async (email, password, done) => {
-        // Fetch user from DynamoDB based on email
-        const params = {
-          TableName: 'users', // Replace with your table name
-          FilterExpression: 'email = :email',
-          ExpressionAttributeValues: {
-            ':email': email,
-          },
-        };
-    
-        try {
-          const result = await db.scan(params).promise();
-          const user = result.Items[0];
-    
-          if (!user || user.password !== password) {
-            return done(null, false, { message: 'Invalid email or password' });
-          }
-    
-          return done(null, user);
-        } catch (error) {
-          return done(error);
-        }
+const { getUserByEmail, getUserById } = require('./user.controller'); // You need to create these functions
+
+passport.use(
+  new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
+    try {
+      const user = await getUserByEmail(email);
+      if (!user || user.password !== password) {
+        return done(null, false, { message: 'Invalid email or password' });
       }
-));
+      return done(null, user);
+    } catch (error) {
+      return done(error);
+    }
+  })
+);
 
 passport.serializeUser((user, done) => {
-    done(null, user.email);
+  done(null, user.id);
 });
 
-passport.deserializeUser(async (email, done) => {
-   // Fetch user from DynamoDB based on email
-   const params = {
-    TableName: 'users', // Replace with your table name
-    FilterExpression: 'email = :email',
-    ExpressionAttributeValues: {
-      ':email': email,
-    },
-  };
-  
+passport.deserializeUser(async (id, done) => {
   try {
-    const result = await db.scan(params).promise();
-    const user = result.Items[0];
+    const user = await getUserById(id);
     done(null, user);
   } catch (error) {
     done(error);
